36dd1addac3eb839db941890321f2e8c513cb887
mazeneldeeb9 <mazeneldeeb9@gmail.com>
2024-09-03 16:45:45 +0300

    Initial Commit

---
+++ graduation project/graduation_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  graduation_projectApp.swift
+//  graduation project
+//
+//  Created by mazen eldeeb on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct graduation_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
